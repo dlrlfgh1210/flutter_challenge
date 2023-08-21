05f67ce1bd69de5ab58a385db94168c2078137ff
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-08-21 22:56:44 +0900

    Initial Screen

---
+++ lib/initial_container.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class InitialContainer extends StatelessWidget {
+  final FaIcon? icon;
+  final String method;
+  final Color bgColor;
+  final Color methodColor;
+  const InitialContainer({
+    super.key,
+    this.icon,
+    required this.method,
+    required this.bgColor,
+    required this.methodColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+          border: Border.all(
+            color: Colors.black38,
+            width: 1,
+          ),
+          color: bgColor,
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 50,
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
+            ),
+            Text(
+              method,
+              style: TextStyle(
+                color: methodColor,
+                fontWeight: FontWeight.w700,
+                fontSize: 17,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/initial_screen.dart
@@ -0,0 +1,153 @@
+import 'package:easy_rich_text/easy_rich_text.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:nomad_flutter_challenge/create_account_screen.dart';
+import 'package:nomad_flutter_challenge/initial_container.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  void onCreateTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const CreateAccountScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(
+                FontAwesomeIcons.twitter,
+                size: 50,
+                color: Colors.blue.shade400,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const Text(
+                "See what's happening \nin the world right now.",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              const InitialContainer(
+                icon: FaIcon(FontAwesomeIcons.google),
+                method: "Continue with Google",
+                bgColor: Colors.white,
+                methodColor: Colors.black,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const InitialContainer(
+                icon: FaIcon(FontAwesomeIcons.apple),
+                method: "Continue with Apple",
+                bgColor: Colors.white,
+                methodColor: Colors.black,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const SizedBox(
+                width: 500,
+                child: Divider(color: Colors.black26, thickness: 2.0),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () => onCreateTap(context),
+                child: const InitialContainer(
+                  method: "Create account",
+                  bgColor: Colors.black,
+                  methodColor: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              EasyRichText(
+                "By signing up, you agree to our Terms, \nPrivacy Policy, and Cookie Use.",
+                defaultStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black,
+                ),
+                patternList: [
+                  EasyRichTextPattern(
+                    targetString: 'Terms',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.blue.shade500,
+                    ),
+                  ),
+                  EasyRichTextPattern(
+                    targetString: 'Privacy Policy',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.blue.shade500,
+                    ),
+                  ),
+                  EasyRichTextPattern(
+                    targetString: 'Cookie Use',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.blue.shade500,
+                    ),
+                  ),
+                ],
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              EasyRichText(
+                "Have an account already?  Log in",
+                defaultStyle: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black,
+                ),
+                patternList: [
+                  EasyRichTextPattern(
+                    targetString: "Log in",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.blue.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
