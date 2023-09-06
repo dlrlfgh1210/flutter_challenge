bd7a69b69b24ae99425e31ecec95cb39fc2914ff
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-09-07 00:06:30 +0900

    PhotoPreviewScreen

---
+++ lib/camera/photo_preview_screen.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+
+class PhotoPreviewScreen extends StatefulWidget {
+  final String imagePath;
+  const PhotoPreviewScreen({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  State<PhotoPreviewScreen> createState() => _PhotoPreviewScreenState();
+}
+
+class _PhotoPreviewScreenState extends State<PhotoPreviewScreen> {
+  bool _savedPhoto = false;
+
+  Future<void> _saveToGallery() async {
+    if (_savedPhoto) return;
+
+    await GallerySaver.saveImage(
+      widget.imagePath,
+    );
+
+    _savedPhoto = true;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Preview'),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(
+              _savedPhoto ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Center(
+          child: Image.file(
+            File(widget.imagePath),
+          ),
+        ),
+      ),
+    );
+  }
+}
